5b7fd55ad9f2a92ab6593962d950059a932ce0da
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2024-10-07 20:52:15 +0200

    register page half

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,12 +1,59 @@
 import React from "react";
 import Topbar from "../../components/topbar/Topbar";
 
+import "./Register.scss";
+
 const Register = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div>
+    <div className="page">
       <Topbar />
 
-      <p>Register</p>
+      <section className="register">
+        <div className="register__container">
+          <p className="register__title">Regístrate en la banca Online</p>
+
+          <form onSubmit={handleSubmit} className="register__form">
+            <input
+              name="name"
+              type="text"
+              placeholder="Nombre"
+              className="register__input"
+            />
+            <input
+              name="email"
+              type="text"
+              placeholder="Email"
+              className="register__input"
+            />
+            <input
+              name="dni"
+              type="text"
+              placeholder="DNI"
+              className="register__input"
+            />
+            <input
+              name="password"
+              type="password"
+              placeholder="Clave"
+              className="register__input"
+            />
+            <input
+              name="password"
+              type="password"
+              placeholder="Repite Clave"
+              className="register__input"
+            />
+
+            <button type="submit" className="register__submit">
+              Registrarse
+            </button>
+          </form>
+        </div>
+      </section>
     </div>
   );
 };
