4e19a625e5da3c6d71bc96c38788a60f7d32c045
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-10-08 11:30:28 +0200

    register form done

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import { useState } from "react";
 import Topbar from "../../components/topbar/Topbar";
 
 import "./Register.scss";
 
 const Register = () => {
+  const initialState = {
+    name: "",
+    email: "",
+    dni: "",
+    password: "",
+    repeat: "",
+  };
+
+  const [formData, setFormData] = useState(initialState);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log(formData);
   };
 
   return (
@@ -21,30 +37,40 @@ const Register = () => {
               name="name"
               type="text"
               placeholder="Nombre"
+              value={formData.name}
+              onChange={handleChange}
               className="register__input"
             />
             <input
               name="email"
               type="text"
               placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
               className="register__input"
             />
             <input
               name="dni"
               type="text"
               placeholder="DNI"
+              value={formData.dni}
+              onChange={handleChange}
               className="register__input"
             />
             <input
               name="password"
               type="password"
               placeholder="Clave"
+              value={formData.password}
+              onChange={handleChange}
               className="register__input"
             />
             <input
-              name="password"
+              name="repeat"
               type="password"
               placeholder="Repite Clave"
+              value={formData.repeat}
+              onChange={handleChange}
               className="register__input"
             />
 
@@ -52,6 +78,10 @@ const Register = () => {
               Registrarse
             </button>
           </form>
+
+          <p className="register__link">
+            <a href="/login">Ya tengo cuenta</a>
+          </p>
         </div>
       </section>
     </div>
