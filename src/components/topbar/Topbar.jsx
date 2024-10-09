c8677a3d495c35fbb5f98f00e04fc260bcea819b
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-10-09 13:12:06 +0200

    topbar done

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import arrowIcon from "../../assets/icons/arrow-left.svg";
+import logoBanner from "../../assets/images/kutxabank-banner-2.jpg";
+import logo from "../../assets/images/logo.jpg";
 
 import "./Topbar.scss";
 
@@ -13,9 +15,12 @@ const Topbar = ({ path, title }) => {
         </a>
       )}
       {title ? (
-        <p className="topbar__text">Topbar</p>
+        <>
+          <p className="topbar__text">{title}</p>
+          <img src={logo} alt="kutxabank logo" className="topbar__logo" />
+        </>
       ) : (
-        <p className="topbar__text">me</p>
+        <img src={logoBanner} alt="kutxabank logo" className="topbar__icon" />
       )}
     </section>
   );
