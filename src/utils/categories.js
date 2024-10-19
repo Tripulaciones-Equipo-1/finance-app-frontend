97a1fae0b0a50c90b3049db4b7a4fc5cb520b167
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-10-19 09:24:04 +0200

    others category

--- src/utils/categories.js
+++ src/utils/categories.js
@@ -2,11 +2,12 @@ import carIcon from "../assets/icons/car.svg";
 import healthIcon from "../assets/icons/pharmacy.svg";
 import funIcon from "../assets/icons/glass-cheers.svg";
 import incomeIcon from "../assets/icons/money-bill-wave.svg";
+import othersIcon from "../assets/icons/others.svg";
 
 export const categories = {
   Ocio: { color: "#e39d07", icon: funIcon },
   Coche: { color: "#076ee3", icon: carIcon },
   Salud: { color: "#c50405", icon: healthIcon },
   Ingreso: { color: "#007a87", icon: incomeIcon },
-  Otros: { color: "#4b4b4b", icon: null },
+  Otros: { color: "#4b4b4b", icon: othersIcon },
 };
