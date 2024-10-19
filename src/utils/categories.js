1cfcbc7c5d54ec9829ae78206b6ca644d74692c5
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-10-19 09:58:33 +0200

    more categories and name changes

--- src/utils/categories.js
+++ src/utils/categories.js
@@ -3,11 +3,14 @@ import healthIcon from "../assets/icons/pharmacy.svg";
 import funIcon from "../assets/icons/glass-cheers.svg";
 import incomeIcon from "../assets/icons/money-bill-wave.svg";
 import othersIcon from "../assets/icons/others.svg";
+import houseIcon from "../assets/icons/house.svg";
 
 export const categories = {
   Ocio: { color: "#e39d07", icon: funIcon },
-  Coche: { color: "#076ee3", icon: carIcon },
+  Transporte: { color: "#076ee3", icon: carIcon },
   Salud: { color: "#c50405", icon: healthIcon },
+  Hogar: { color: "#9607e3", icon: houseIcon },
   Ingreso: { color: "#007a87", icon: incomeIcon },
+  Nominas: { color: "#007a87", icon: incomeIcon },
   Otros: { color: "#4b4b4b", icon: othersIcon },
 };
